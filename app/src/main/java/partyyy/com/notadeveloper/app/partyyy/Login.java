ed13a8db7f5c68707cbc19825b42d87d0c344124
vijay krishna <krsnvijay@gmail.com>
2017-02-22 19:42:19 +0530

    Initial commit

---
+++ app/src/main/java/partyyy/com/notadeveloper/app/partyyy/Login.java
@@ -0,0 +1,94 @@
+package partyyy.com.notadeveloper.app.partyyy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+
+import java.util.Arrays;
+
+public class Login extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 9001;
+    private static boolean calledAlready = false;
+    private final String TAG = "Login";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.AppTheme_NoActionBar)
+                        .setLogo(R.drawable.ic_confettilarge)
+                        .setIsSmartLockEnabled(true)
+                        // .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                        .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build()))
+
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                startActivity(new Intent(Login.this,MainActivity.class));
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    Snackbar.make(findViewById(android.R.id.content), R.string.sign_in_cancelled, Snackbar.LENGTH_SHORT).show();
+                    startActivity(new Intent(Login.this,MainActivity.class));
+                    finish();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Snackbar snackbar = Snackbar
+                            .make(findViewById(android.R.id.content), R.string.no_internet_connection, Snackbar.LENGTH_INDEFINITE)
+                            .setAction("RETRY", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    startActivity(new Intent(Login.this,MainActivity.class));
+                                    finish();
+
+                                }
+                            });
+
+                    snackbar.show();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Snackbar.make(findViewById(android.R.id.content), R.string.unknown_error, Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+
+            Snackbar.make(findViewById(android.R.id.content), R.string.unknown_error, Snackbar.LENGTH_SHORT).show();
+        }
+
+        Toast.makeText(Login.this, R.string.unknown_response,
+                Toast.LENGTH_LONG).show();
+    }
+}
+
+
