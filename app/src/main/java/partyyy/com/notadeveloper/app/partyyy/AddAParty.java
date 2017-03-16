b7f83610b1b2479120631763a66737df61b723f9
krsnvijay <krsnvijay@gmail.com>
2017-03-16 15:08:49 +0530

    Time

--- app/src/main/java/partyyy/com/notadeveloper/app/partyyy/AddAParty.java
+++ app/src/main/java/partyyy/com/notadeveloper/app/partyyy/AddAParty.java
@@ -156,26 +156,7 @@ public class AddAParty extends AppCompatActivity implements DatePickerDialog.OnD
 
 
 
-    public static class TimePickerFragment extends DialogFragment
-            implements TimePickerDialog.OnTimeSetListener {
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
-
-        }
-    }
 
     public static class DatePickerFragment extends DialogFragment {
 
